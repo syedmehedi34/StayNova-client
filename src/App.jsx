7ce2a0e62078aa43ccdf69f7b3de850d98eed749
syedmehedi34 <syedmeehdi34@gmail.com>
2025-08-01 14:16:27 +0600

    designed the navbar icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { useLocation } from "react-router-dom";
 
 const App = () => {
-  return <div></div>;
+  const isOwnerPath = useLocation().pathname.includes("/owner");
+
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+      {/*  */}
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      hero will be here
+      {/*  */}
+    </div>
+  );
+};
+
+export default Hero;
